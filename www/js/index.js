9cf2be0b92b069e21c8974e674e25581df8f9bc6
Mark Njoroge <marknnjorogeh@gmail.com>
2021-07-19 20:14:40 +0300

    almost done

--- www/js/index.js
+++ www/js/index.js
@@ -1,58 +1,52 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
-var propName, propNumber, propType, leaseType, location
-var bedrooms, bathrooms, size, price, amenities, description
-
-var clicked = false
-
-function confirmDetails() {
-
-    if(!clicked) {
-
-        document.getElementById('instructions').innerHTML = "Please confirm the details and press"
-        clicked = true
-        
-    } else {
-
-        propName = document.getElementById('name').value
-        propNumber = document.getElementById('name').value
-        propType = document.getElementById('name').value
-        leaseType = document.getElementById('name').value
-        location = document.getElementById('name').value
-        bedrooms = document.getElementById('name').value
-        bathrooms = document.getElementById('name').value
-        size = document.getElementById('name').value
-        price = document.getElementById('name').value
-        amenities = document.getElementById('name').value
-        description = document.getElementById('name').value
-
-    }
-}
-
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+}
+
+var propName, propNumber, propType, leaseType, location, propAge, leaseDuration;
+var bedrooms, bathrooms, size, price, amenities, description;
+
+var clicked = false;
+
+var db = null;
+
+document.getElementById('submitProp').addEventListener("click", check)
+
+function check() {
+    if(!clicked) {
+
+        document.getElementById('instructions').innerHTML = "Please confirm the details to continue"
+        clicked = true
+
+        var submit = document.getElementById('submitProp')
+
+        submit.href = "#proptype"
+
+    } else {
+
+        propName = document.getElementById('propname').value
+        propNumber = document.getElementById('propnumber').value
+        propType = document.getElementById('proptype').value
+        leaseType = document.getElementById('leasetype').value
+        propAge = document.getElementById('propage').value
+        leaseDuration = document.getElementById('leaseduration').value
+        location = document.getElementById('proplocation').value
+        bedrooms = document.getElementById('bedrooms').value
+        bathrooms = document.getElementById('bathrooms').value
+        size = document.getElementById('size').value
+        price = document.getElementById('askprice').value
+        amenities = document.getElementById('localamenities').value
+        description = document.getElementById('propdescription').value
+
+
+        // document.addEventListener('deviceready', function() {
+        //     db = window.sqlitePlugin.openDatabase({
+        //         name: 'my.db',
+        //         location: 'default',
+        //     });
+        // });
+    }
 }
